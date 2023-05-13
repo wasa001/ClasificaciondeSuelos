9e2282ab0a23d753b41edb1bc900811ccf676e86
wasa001 <132782697+wasa001@users.noreply.github.com>
2023-05-13 09:54:05 -0500

    Inputs.py
    Segunda Version

---
+++ funciones/Inputs.py
@@ -0,0 +1,21 @@
+'''INPUT'''
+def DatosIniciales():
+  Ll = 50                         #Captura el valor del Limite Liquido
+  LP=  45                  #Captura el valor del Indice de Plasticidad
+  IP = Ll - LP 
+
+  #INTRODUCE LOS DATOS PARA LA GRANULOMETRIA
+
+  #Crea la lista de %Pasa
+  #POR FAVOR MODIFIQUE SU GRANULOMETRIA
+  tamiz1ymedio = 100
+  Tamiz1 = 100
+  Tamiz3cuartos = 100
+  Tamiz3octavos = 100
+  No4 = 95
+  No10 = 80
+  No20 = 51
+  No40 = 33
+  No60 = 13
+  No140 =6
+  No200 =3
