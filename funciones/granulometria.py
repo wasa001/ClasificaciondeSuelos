fce7f0ca92014eb29ee6b633ac1367bb255a3500
wasa001 <132782697+wasa001@users.noreply.github.com>
2023-05-13 10:09:43 -0500

    Update granulometria.py

--- funciones/granulometria.py
+++ funciones/granulometria.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from Funciones.Inputs import DatosIniciales
+from funciones.Inputs import DatosIniciales
 
 #Se grafica la granulometria
 def Granulometria():
